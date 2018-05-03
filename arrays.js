2e7daf119120681e330732786e51abf979098a67
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-05-03 23:28:07 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(array,element){
+  array2 = [];
+  array2.unshift(element,...array);
+  return array2;
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element);
+  return array;
+}
+function addElementToEndOfArray(array,element){
+  array2 = [];
+  array2.push(...array,element);
+  return array2
+}
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element);
+  return array
+}
+function accessElementInArray(array,index){
+   return array[index];
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length - 1);
+}
